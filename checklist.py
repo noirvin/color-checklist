221baa889811df7f7700b27215fe02c9bd2bf0c3
Arvin Seifipour <arvinseifipour@Arvins-MacBook-Pro.local>
2019-09-02 19:46:07 -0700

    added read, destroy, update functionality to select

--- checklist.py
+++ checklist.py
@@ -36,11 +36,21 @@ def select(function_code):
     # Print all items
     elif function_code == "P":
         list_all_items()
-        
 
+    elif function_code == "Q":
+        return false
+    #modify index specified item
+    elif function_code == "U":
+        itemIndex = user_input("input index")
+        updatedItem = user_input("input new item")
+        update(itemIndex,updatedItem)
+    elif function_code == "D":
+        itemIndex = user_input("Enter index")
+        destroy(itemIndex)    
     # Catch all
     else:
         print("Unknown Option")
+        return true
 
 def user_input(prompt):
     user_input = input(prompt)
@@ -69,4 +79,4 @@ running = True
 while running:
     selection = user_input(
         "Press C to add to list, R to Read from list and P to display list")
-    select(selection)
+    running = select(selection)
